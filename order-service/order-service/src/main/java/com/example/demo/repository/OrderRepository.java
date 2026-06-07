7a3744633744765f2c0dc902770b0e2e0db836ac
leodiel <leandro.jtl.tech@gmail.com>
2026-06-07 02:42:33 -0300

    feat(Día2): agregar findByStatus en Repository

--- order-service/order-service/src/main/java/com/example/demo/repository/OrderRepository.java
+++ order-service/order-service/src/main/java/com/example/demo/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+List<Order> findByStatus(String status);
 }
